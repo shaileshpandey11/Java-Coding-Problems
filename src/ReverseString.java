9312260c62a085a5669f14a20c5ed26654b2cdb8
Sujay Sawant <sujayvsawant@gmail.com>
2019-10-26 22:33:45 +0530

    Added method to reverse string

--- src/ReverseString.java
+++ src/ReverseString.java
@@ -2,15 +2,21 @@
  * Created by Shailesh on 12/26/2016.
  */
 public class ReverseString {
-    public static void main(String args[]){
-        String input = "Inception";
-        char[] tempChar = input.toCharArray();
-        char[] result = new char[tempChar.length];
+	
+	public static void main(String args[]) {
+		String input = "Inception";
+		char[] tempChar = input.toCharArray();
+		char[] result = new char[tempChar.length];
 
-        for(int i=0;i<tempChar.length;i++){
-            result[i]=tempChar[tempChar.length-i-1];
-        }
-        System.out.println("Reverse String: "+new String(result));
+		for (int i = 0; i < tempChar.length; i++) {
+			result[i] = tempChar[tempChar.length - i - 1];
+		}
+		System.out.println("Reverse String: " + new String(result));
+		System.out.println("Reverse String: " + reverse(input));
+	}
+	
+	private static String reverse(String str) {
+    	return new StringBuilder(str).reverse().toString();
     }
 
 }
